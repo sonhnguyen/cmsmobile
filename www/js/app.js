9af62cdf78a5661e938ba67cc1511181f502fd2f
tqduy <tqduy@apcs.vn>
2015-12-17 09:03:42 +0700

    lỗi: ng-app = "starter" is not defined

--- www/js/app.js
+++ www/js/app.js
@@ -31,55 +31,32 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Each state's controller can be found in controllers.js
   $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+     .state("menu", {
+            url: "/menu",
+            templateUrl: "view/menu.html",
+            controller: "menuController"
+        })
+        .state("deal", {
+            url: "/deal",
+            templateUrl: "view/deal.html",
+            controller: "dealController"
+        })
+        .state("news", {
+            url: "/news",
+            templateUrl: "view/news.html",
+            controller: "newsController"
+        })
+        .state("recipe", {
+            params: {
+                //blogid:{}
+            },
+            url: "/recipe",
+            templateUrl: "view/recipe.html",
+            controller: "recipeController",
+            // data: {'blogid':'blogid'}
+    });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/');
 
 });
--- www/js/controller/mainController.js
+++ www/js/controller/mainController.js
@@ -1,10 +1,32 @@
 starter.controller('mainController', ['$scope','$state','$ionicPopover','$ionicPopup','$ionicLoading','$ionicModal','$ionicHistory',function ($scope,$state,$ionicPopover,$ionicPopup,$ionicLoading,$ionicModal,$ionicHistory) {
 
+$scope.viewMenu = function($event){
+    $('#menuTab').addClass('active');
+    $('#dealTab').removeClass('active');
+    $('#newsTab').removeClass('active');
+    $('#recipeTab').removeClass('active');
+  };
+
+$scope.viewDeal = function($event){
+    $('#menuTab').removeClass('active');
+    $('#dealTab').addClass('active');
+    $('#newsTab').removeClass('active');
+    $('#recipeTab').removeClass('active');
+  };
+  $scope.viewNews = function($event){
+    $('#menuTab').removeClass('active');
+    $('#dealTab').removeClass('active');
+    $('#newsTab').addClass('active');
+    $('#recipeTab').removeClass('active');
+  };
+  $scope.viewRecipe = function($event){
+    $('#menuTab').removeClass('active');
+    $('#dealTab').removeClass('active');
+    $('#newsTab').removeClass('active');
+    $('#recipeTab').addClass('active');
+  };
 
 
 
 
-
-
-
-}
+}]);
