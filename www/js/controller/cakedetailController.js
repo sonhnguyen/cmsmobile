45789618745658715bee9494a56bb4c4505f9989
Son Nguyen <hoangson.2411@gmail.com>
2016-01-29 00:38:49 +0700

    done

--- www/js/controller/cakedetailController.js
+++ www/js/controller/cakedetailController.js
@@ -1,49 +1,56 @@
-cms.controller('cakedetailController', ['$scope', '$state', '$ionicPopover', '$ionicPopup',
-    '$ionicLoading', '$ionicModal', '$ionicHistory', '$stateParams', 'CakeService',
-    function($scope, $state, $ionicPopover, $ionicPopup, $ionicLoading, $ionicModal, $ionicHistory, $stateParams, CakeService) {
-        cakeId = $stateParams['cakeId'];
-        console.log(cakeId);
+cms.controller('cakedetailController',
 
-        CakeService.getCakeById(cakeId).then(
-            function(data) {
-                var cake = data;
-                $scope.myCake = cake;
-                console.log(cake);
+    ['$scope', '$state', '$ionicPopover', '$ionicPopup',
+        '$ionicLoading', '$ionicModal', '$ionicHistory', '$stateParams',
+        'CakeService', '$ionicSlideBoxDelegate', '$ionicScrollDelegate',
+        function($scope, $state, $ionicPopover, $ionicPopup, $ionicLoading, $ionicModal, $ionicHistory, $stateParams, CakeService, $ionicSlideBoxDelegate, $ionicScrollDelegate) {
+            cakeId = $stateParams['cakeId'];
+            $scope.zoomMin = 1;
+
+            CakeService.getCakeById(cakeId).then(
+                function(data) {
+                    var cake = data;
+                    $scope.myCake = cake;
+                    $scope.cakeDesc = $scope.myCake.Description;
+                    $scope.images = [];
+                    $scope.images.push($scope.myCake.Gallery);
+                    $scope.images.push($scope.myCake.Image);
+                    $scope.myCake.Gallery['newkey'] = $scope.myCake.Image
+                }
+            )
+            $scope.showImages = function(index, parentIndex) {
+                $scope.activeSlide = index;
+                console.log(index);
+                $scope.eventImg = $scope.myCake.Gallery;
+                console.log("event img", $scope.eventImg);
+                $scope.showModal('view/template/gallery-zoomview.html');
+            };
+
+            $scope.showModal = function(templateUrl) {
+                $ionicModal.fromTemplateUrl(templateUrl, {
+                    scope: $scope,
+                    backdropClickToClose: true
+
+                }).then(function(modal) {
+                    $scope.modal = modal;
+                    $scope.modal.show();
+                });
             }
-        )
-        $scope.showImages = function(index, parentIndex) {
-            $scope.activeSlide = index;
-            console.log(index);
-            $scope.eventImg = $scope.myCake.Gallery;
-            console.log($scope.myCake.Gallery);
-            $scope.showModal('view/template/gallery-zoomview.html');
-        };
 
-        $scope.showModal = function(templateUrl) {
-            $ionicModal.fromTemplateUrl(templateUrl, {
-                scope: $scope,
-                backdropClickToClose: true
+            $scope.closeModal = function() {
+                $scope.modal.hide();
+                $scope.modal.remove()
+            };
 
-            }).then(function(modal) {
-                $scope.modal = modal;
-                $scope.modal.show();
-            });
+            $scope.updateSlideStatus = function(slide) {
+                var zoomFactor = $ionicScrollDelegate.$getByHandle('scrollHandle' + slide).getScrollPosition().zoom;
+                if (zoomFactor == $scope.zoomMin) {
+                    $ionicSlideBoxDelegate.enableSlide(true);
+                } else {
+                    $ionicSlideBoxDelegate.enableSlide(false);
+                }
+            };
+
+
         }
-
-        $scope.closeModal = function() {
-            $scope.modal.hide();
-            $scope.modal.remove()
-        };
-
-        $scope.updateSlideStatus = function(slide) {
-            var zoomFactor = $ionicScrollDelegate.$getByHandle('scrollHandle' + slide).getScrollPosition().zoom;
-            if (zoomFactor == $scope.zoomMin) {
-                $ionicSlideBoxDelegate.enableSlide(true);
-            } else {
-                $ionicSlideBoxDelegate.enableSlide(false);
-            }
-        };
-
-
-    }
-]);
+    ]);
